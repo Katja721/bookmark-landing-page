84638e1ef37ea8b2683c0631ee12f2bab443cdd6
Katja721 <katja.debevc72@gmail.com>
2022-06-29 20:56:31 +0200

    hover states

--- script.js
+++ script.js
@@ -1,1 +1,72 @@
 "use strict";
+const [...featureItems] = document.querySelectorAll(".featureItem");
+const featuresMenu = document.querySelector(".featuresMenu");
+const [...features] = document.querySelectorAll(".feature");
+const [...questions] = document.querySelectorAll(".question");
+const [...answers] = document.querySelectorAll(".answer");
+const [...arrows] = document.querySelectorAll(".arrow-icon");
+const emailInput = document.querySelector(".emailInput");
+const contactBtn = document.querySelector(".contactBtn");
+const changeNavItem = function (click) {
+  featureItems.forEach((item) => item.classList.remove("activeFeature"));
+  click.closest(".featureItem").classList.add("activeFeature");
+};
+const toggleFeaturesText = function (element) {
+  const index = featureItems.indexOf(element);
+  features.forEach((item) => item.classList.add("unactive"));
+  features[index].classList.remove("unactive");
+};
+const emailChange = function (borderRadius, border) {
+  emailInput.style.borderBottomLeftRadius = borderRadius;
+  emailInput.style.borderBottomRightRadius = borderRadius;
+  emailInput.style.border = border;
+};
+featuresMenu.addEventListener("click", function (e) {
+  const click = e.target;
+  if (!click.closest(".featureItem")) return;
+  changeNavItem(click);
+
+  featureItems.forEach((element) => {
+    if (!element.classList.contains("activeFeature")) return;
+    toggleFeaturesText(element);
+  });
+});
+questions.forEach((question) =>
+  question.addEventListener("click", function (e) {
+    questions.forEach(
+      (question) =>
+        (question.style.borderBottom = "1px solid var(--grayish-blue)")
+    );
+    const answer = question.closest(".questionBox").querySelector(".answer");
+    const arrow = question.querySelector(".arrow-icon");
+    answers.forEach((answer) => answer.classList.add("hidden"));
+
+    arrows.forEach((arrow) => {
+      arrow.style.color = "var(--soft-blue)";
+      arrow.style.transform = "rotate(360deg)";
+    });
+    answer.classList.remove("hidden");
+    question.style.borderBottom = "none";
+    answer.style.borderBottom = "1px solid var(--grayish-blue)";
+    arrow.style.color = "var(--soft-red)";
+    arrow.style.transform = "rotate(180deg)";
+  })
+);
+contactBtn.addEventListener("click", function (e) {
+  const errorIcon = document.querySelector(".error-icon");
+  const errorParagraph = document.querySelector(".error-paragraph");
+  const email = emailInput.value;
+
+  const emailRegex = RegExp(
+    /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+  );
+  console.log(email);
+  errorIcon.classList.add("hidden");
+  errorParagraph.classList.add("hidden");
+  emailChange("5px", "none");
+  if (emailRegex.test(email)) return;
+  errorIcon.classList.remove("hidden");
+  emailChange("0px", "1px solid var(--soft-red)");
+
+  errorParagraph.classList.remove("hidden");
+});
